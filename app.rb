70fd5e75120f1f4d14a6d96b7a97dee4574fad99
Cristiano Betta <cbetta@gmail.com>
2016-10-29 23:33:56 +0100

    Parsing request

--- app.rb
+++ app.rb
@@ -5,14 +5,21 @@ post '/metastatus' do
   query_json = JSON.parse(request.body.read.to_s)
   query = AlexaRubykit.build_request(query_json)
 
-  process(query) if query.type == 'StatusCheck'
+  process(query) if query.type == 'INTENT_REQUEST'
 end
 
 private
 
 def process query
+  case query.intent['name']
+  when "StatusCheck"
+    service = query.slots['Service']['value']
+    status_check service
+  end
+end
+
+def status_check service
   response = AlexaRubykit::Response.new
-  puts query.slots.inspect
-  response.add_speech('Checking now')
+  response.add_speech("Checking #{service}")
   response.build_response
 end
