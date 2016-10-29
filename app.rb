27f6b3524b1a64b70dfd60d7a9a20db8d684da30
Cristiano Betta <cbetta@gmail.com>
2016-10-30 00:05:50 +0100

    Add Digital Ocean skill

--- app.rb
+++ app.rb
@@ -2,12 +2,21 @@ require 'sinatra'
 require 'alexa_rubykit'
 require 'json'
 require 'open-uri'
+require 'rss'
 
 post '/metastatus' do
   query_json = JSON.parse(request.body.read.to_s)
   query = AlexaRubykit.build_request(query_json)
 
   process(query) if query.type == 'INTENT_REQUEST'
+end
+
+get '/github' do
+  respond status_of_github
+end
+
+get '/digital_ocean' do
+  status_of_digital_ocean
 end
 
 private
@@ -17,7 +26,6 @@ def process query
   when "StatusCheck"
     service = query.slots['Service']['value']
     respond status_of(service)
-
   end
 end
 
@@ -26,13 +34,13 @@ def status_of service
   when "github"
     status_of_github
   when "digital ocean"
-    "Sorry I am not set up to deal with Digital Ocean just yet"
+    status_of_digital_ocean
   end
 end
 
 def status_of_github
-  result = open('https://status.github.com/api/last-message.json').read
-  status = JSON.parse(result)
+  response = open('https://status.github.com/api/last-message.json').read
+  status = JSON.parse(response)
   level = status['status']
   body = status['body']
 
@@ -44,6 +52,18 @@ def status_of_github
   end
 end
 
+def status_of_digital_ocean
+  page = open('https://status.digitalocean.com/').read
+  rss =  open('https://status.digitalocean.com/rss').read
+  feed = RSS::Parser.parse(rss)
+  latest_update = feed.items.first.title
+
+  if page.include?('All Systems Operational')
+    "Digital Ocean is fully operational. The latest update is: #{latest_update}"
+  else
+    "Digital Ocean is having some issues. The latest update is: #{latest_update}"
+  end
+end
 
 def respond answer
   response = AlexaRubykit::Response.new
