daa3eeb693ed90b28e7fc07cbb638e6203e17f75
Cristiano Betta <cbetta@gmail.com>
2016-10-29 22:53:01 +0100

    Switch to post

--- app.rb
+++ app.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 require 'alexa_rubykit'
 
-get '/metastatus' do
+post '/metastatus' do
   response = AlexaRubykit::Response.new
   response.add_speech('Ruby is running ready!')
   response.build_response
