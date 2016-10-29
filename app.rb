093cce30956a3edd26da75a5587f8bed1dbda8b7
Cristiano Betta <cbetta@gmail.com>
2016-10-29 23:36:45 +0100

    Add an actual Github check

--- app.rb
+++ app.rb
@@ -14,12 +14,22 @@ def process query
   case query.intent['name']
   when "StatusCheck"
     service = query.slots['Service']['value']
-    status_check service
+    respond status_of(service)
+
   end
 end
 
-def status_check service
+def status_of service
+  case service
+  when "github"
+    result = open('https://status.github.com/api/status.json').read
+    status = JSON.parse(result)['status']
+    "The status of GitHub is #{status}"
+  end
+end
+
+def respond answer
   response = AlexaRubykit::Response.new
-  response.add_speech("Checking #{service}")
+  response.add_speech(answer)
   response.build_response
 end
